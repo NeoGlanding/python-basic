2076492cae9e6978de92c9b6dc694e149be00203
NeoGlanding <neoglanding@protonmail.com>
2021-08-30 12:38:37 +0700

    variable with string and integer

--- basic.py
+++ basic.py
@@ -1,1 +1,10 @@
-print('Hello World')
+print('Hello World');
+
+greeting = "Hello";
+name = "Ihsan"
+print(greeting + name)
+age = 10;
+
+print(type(age))
+print(type(name));
+print(type(greeting));
